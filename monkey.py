d54efe822191708188dc10ea6f2f5ced8abb0e4c
kawaii <nyaa@openmailbox.org>
2016-01-07 11:03:00 -0500

    added more words

--- monkey.py
+++ monkey.py
@@ -1,5 +1,4 @@
 from random import randint
-randnum = randint(0,19)
-wordlib = ["tomato ", "brick ", "cheese ", "banana ", "eagle ", "oil ", "america ", "guns ", "bullets ", "potato ", "jews ", "aphrodisiac ", "bullseye ", "heretic ", "xenomorph ", "cuddle ", "bubble ", "insomnia ", "entropy ", "dinosaur " ]
+randnum = randint(0,29)
+wordlib = ["tomato ", "brick ", "cheese ", "banana ", "eagle ", "oil ", "america ", "guns ", "bullets ", "potato ", "jews ", "aphrodisiac ", "bullseye ", "heretic ", "xenomorph ", "cuddle ", "bubble ", "insomnia ", "entropy ", "dinosaur ", "terminal ", "python ", "oasis ", "snow ", "animu ", "dank ", "weeaboo ", "imageboard ", "nano ", "shakespeare ", "kawaii ", "neko ", "aubergine ", "eggplant ", "oppression ", "monkey ", "shoe ", "politics ", "tumblr ", "imageboard"]
 print wordlib[randnum]
-

56a0087e96d42029287ca34e29950360a0654bc6
kawaii <nyaa@openmailbox.org>
2016-01-07 12:35:59 -0500

    prints whole list and list is shuffled

--- monkey.py
+++ monkey.py
@@ -1,4 +1,8 @@
+from random import shuffle
 from random import randint
 randnum = randint(0,29)
-wordlib = ["tomato ", "brick ", "cheese ", "banana ", "eagle ", "oil ", "america ", "guns ", "bullets ", "potato ", "jews ", "aphrodisiac ", "bullseye ", "heretic ", "xenomorph ", "cuddle ", "bubble ", "insomnia ", "entropy ", "dinosaur ", "terminal ", "python ", "oasis ", "snow ", "animu ", "dank ", "weeaboo ", "imageboard ", "nano ", "shakespeare ", "kawaii ", "neko ", "aubergine ", "eggplant ", "oppression ", "monkey ", "shoe ", "politics ", "tumblr ", "imageboard"]
-print wordlib[randnum]
+wordlib = ["tomato", "brick", "cheese", "banana", "eagle", "oil", "america", "guns", "bullets", "potato", "jews", "aphrodisiac", "bullseye", "heretic", "xenomorph", "cuddle", "bubble", "insomnia", "entropy", "dinosaur", "terminal", "python", "oasis", "snow", "animu", "dank", "weeaboo", "imageboard", "nano", "shakespeare", "kawaii", "neko", "aubergine", "eggplant", "oppression", "monkey", "shoe", "politics", "tumblr", "imageboard"]
+shuffle(wordlib)
+for wordlib[randnum] in wordlib:
+	print wordlib[randnum]
+

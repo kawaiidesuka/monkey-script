5303e51561dec09b8ff54deeca14788334cd4d0c
kawaii <nyaa@openmailbox.org>
2016-01-07 10:54:31 -0500

    added monkey script

---
+++ monkey.py
@@ -0,0 +1,5 @@
+from random import randint
+randnum = randint(0,19)
+wordlib = ["tomato ", "brick ", "cheese ", "banana ", "eagle ", "oil ", "america ", "guns ", "bullets ", "potato ", "jews ", "aphrodisiac ", "bullseye ", "heretic ", "xenomorph ", "cuddle ", "bubble ", "insomnia ", "entropy ", "dinosaur " ]
+print wordlib[randnum]
+
